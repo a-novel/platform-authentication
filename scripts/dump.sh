be22294c32147e4622c5005ae64c5840bd662a39
Kushuh <geoffroy.vincent@agorastoryverse.com>
2026-04-02 13:42:57 +0200

    chores(tolgee): optimize dump

--- scripts/dump.sh
+++ scripts/dump.sh
@@ -2,13 +2,18 @@
 
 set -e
 
-dumpCmd="pg_dump -U postgres --clean --if-exists --role=postgres postgres"
-
 APP_NAME="auth-translations"
+
+EXEC_CMD=""
 
 if [ "$IS_CI" == "true" ]
 then
-    docker exec "${APP_NAME}"-tolgee-db-1 $dumpCmd > builds/tolgee.database.dump
+    EXEC_CMD="docker exec ${APP_NAME}-tolgee-db-1"
 else
-    podman exec "${APP_NAME}"_tolgee-db_1 $dumpCmd > builds/tolgee.database.dump
+    EXEC_CMD="podman exec ${APP_NAME}_tolgee-db_1"
 fi
+
+$EXEC_CMD psql -U postgres -d postgres -c "DELETE FROM public.activity_describing_entity;"
+$EXEC_CMD psql -U postgres -d postgres -c "DELETE FROM public.activity_modified_entity;"
+$EXEC_CMD psql -U postgres -d postgres -c "DELETE FROM public.activity_revision;"
+$EXEC_CMD pg_dump -U postgres --clean --if-exists --role=postgres postgres > builds/tolgee.database.dump
